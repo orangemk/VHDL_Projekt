0b110088b64d773ecbe4b824f64dce873f1b06fc
DoBa123 <dominik_back@web.de>
2018-04-12 11:38:32 +0200

    Version 0.1

---
+++ XADC_Modul/XADC_Modul.srcs/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.v
@@ -0,0 +1,40 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+// Date        : Thu Apr 12 10:04:02 2018
+// Host        : DESKTOP-I49FJT5 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/Dominik/Vivado_Workspace/XADC_Modul/XADC_Modul.srcs/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.v
+// Design      : xadc_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-3
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module xadc_wiz_0(daddr_in, den_in, di_in, dwe_in, do_out, drdy_out, 
+  dclk_in, reset_in, vauxp3, vauxn3, busy_out, channel_out, eoc_out, eos_out, vccaux_alarm_out, 
+  vccint_alarm_out, user_temp_alarm_out, alarm_out, vp_in, vn_in)
+/* synthesis syn_black_box black_box_pad_pin="daddr_in[6:0],den_in,di_in[15:0],dwe_in,do_out[15:0],drdy_out,dclk_in,reset_in,vauxp3,vauxn3,busy_out,channel_out[4:0],eoc_out,eos_out,vccaux_alarm_out,vccint_alarm_out,user_temp_alarm_out,alarm_out,vp_in,vn_in" */;
+  input [6:0]daddr_in;
+  input den_in;
+  input [15:0]di_in;
+  input dwe_in;
+  output [15:0]do_out;
+  output drdy_out;
+  input dclk_in;
+  input reset_in;
+  input vauxp3;
+  input vauxn3;
+  output busy_out;
+  output [4:0]channel_out;
+  output eoc_out;
+  output eos_out;
+  output vccaux_alarm_out;
+  output vccint_alarm_out;
+  output user_temp_alarm_out;
+  output alarm_out;
+  input vp_in;
+  input vn_in;
+endmodule
